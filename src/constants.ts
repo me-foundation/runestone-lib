d3ee72dab42c7cccef2c43099563b52aa641517d
Steven Lee <steven.lee@magiceden.io>
2024-03-22 02:14:46 -0700

    Make max mint limit u64::MAX (#12)

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import * as bitcoin from 'bitcoinjs-lib';
 import { u128 } from './u128';
 
 export const MAX_DIVISIBILITY = 38;
-export const MAX_LIMIT = u128(1n << 64n);
+export const MAX_LIMIT = u128((1n << 64n) - 1n);
 export const RESERVED = u128(6402364363415443603228541259936211926n);
 export const SUBSIDY_HALVING_INTERVAL = 210_000;
 export const MAX_SCRIPT_ELEMENT_SIZE = 520;
